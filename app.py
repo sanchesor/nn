3399e848379aaa36b5f8c9149ac83600199aabb5
sanches@toshiba <sanchesor@gmail.com>
2018-02-03 23:11:52 +0100

    forward checked

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import network as n
 
-n1 = n.Network([30,3,2])
+n1 = n.Network([2,2,1])
 
+input = [1,2]
+n1.forward(input)
 
+print('input', input)
 n1.show()
--- network.py
+++ network.py
@@ -4,15 +4,30 @@ class Network:
 
     def __init__(self, sizes):
         self.sizes = sizes;
-        self.biases = [np.random.randn(y,1) for y in self.sizes[1:]]
+        self.num_layers = len(sizes)
+        self.biases = [np.random.randn(y) for y in self.sizes[1:]]
         self.weights = [np.random.randn(y,x)
             for x,y in zip(self.sizes[:-1], self.sizes[1:]) ]
+        self.nets = [np.zeros(y) for y in self.sizes[1:]]
+        self.outputs = [np.zeros(y) for y in self.sizes[1:]]
+
+    def forward(self, input):
+        l_input = input
+        for l in range(0, self.num_layers-1):
+            self.nets[l] = np.dot(self.weights[l], l_input) + self.biases[l]
+            self.outputs[l] = self.activation(self.nets[l])
+            l_input = self.outputs[l]
+
+    def activation(self, x):
+        return 1.0/(1.0 + np.exp(-x))
 
     def show(self):
         np.set_printoptions(precision=2)
         for l in range(0, len(self.sizes)-1):
             print('=== layer',l+1, ' ===')
-            print(self.weights[l])
-            print(self.biases[l])
+            print('weights',self.weights[l])
+            print('biases',self.biases[l])
+            print('net',self.nets[l])
+            print('out',self.outputs[l])
 
         np.set_printoptions(precision=8)
